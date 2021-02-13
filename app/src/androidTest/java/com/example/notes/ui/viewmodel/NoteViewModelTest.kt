2ff2b79daa920811bd17a6930e0ece51f4d35cc9
lddconf <lddconf@gmail.com>
2021-02-13 20:12:25 +0300

    Плановая фиксация

---
+++ app/src/androidTest/java/com/example/notes/ui/viewmodel/NoteViewModelTest.kt
@@ -0,0 +1,69 @@
+package com.example.notes.ui.viewmodel
+
+import android.content.Intent
+import androidx.lifecycle.MutableLiveData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import com.example.notes.R
+import com.example.notes.model.Note
+import com.example.notes.model.NotesRepo
+import com.example.notes.ui.activities.NoteViewActivity
+import com.example.notes.ui.activities.NoteViewState
+import io.mockk.*
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Rule
+import org.junit.Test
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+
+
+class NoteViewModelTest {
+    @get:Rule
+    val activityTestRule = ActivityTestRule(NoteViewActivity::class.java, true, false)
+
+    private val viewModel: NoteViewModel = spyk(NoteViewModel(mockk<NotesRepo>()))
+    private val viewStateLiveData = MutableLiveData<NoteViewState>()
+    private val testNote = Note(uid = "123", title = "TestNoteTitle", text = "TestNoteText")
+
+    @Before
+    fun setUp() {
+        startKoin {
+            modules()
+        }
+
+        loadKoinModules(
+            module {
+                viewModel { viewModel }
+            }
+        )
+
+        every { viewModel.viewState() } returns viewStateLiveData
+        every { viewModel.loadNote(any<String>()) } just runs
+        every { viewModel.saveChanges(any<Note>()) } just runs
+
+        activityTestRule.launchActivity(Intent().apply {
+            putExtra("NoteViewActivity.extra.NOTE", testNote.uid)
+        })
+
+    }
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun should_shown_color_picker() {
+        onView(withId(R.id.action_color_pick)).perform(click())
+        onView(withText(R.string.color_selection_title)).check(matches(isDisplayed()))
+    }
+
+}
